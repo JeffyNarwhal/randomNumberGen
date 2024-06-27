24b7eaeb11d8924e9eafab66d0a76f7a0f40575f
JeffyNarwhal <tyler@johnsonre.com>
2024-06-27 14:29:55 -0400

    Changed info, added exception

--- NumGen.py
+++ NumGen.py
@@ -7,34 +7,32 @@ randomlist = []
 copylist = []
 
 layout = [
-
     [sg.Text("Random Number Generator"), sg.Button(
         "Info", key="Info")],
-    [sg.Input(size=(15, 1)),
-     sg.Input(size=(15, 1)), sg.Button("Copy", key="Copy")],
+    [sg.Input("1", size=(15, 1)),
+     sg.Input("10", size=(15, 1)), sg.Button("Copy", key="Copy")],
     [sg.Text(key="Text")],
     [sg.Button("Generate", key="Gen"), sg.Button("Quit"), sg.Text(
         "Numbers Generated:"), sg.Input("1", size=(3, 1))]
-
 ]
 
-
-window = sg.Window("Number Gen", layout)
-
+window = sg.Window("Number Generator", layout)
 
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
     if event == "Info":
-        webbrowser.open('https://www.youtube.com/channel/UCAOMblfh6EkuQQByFFgpCcQ', new=1)
+        webbrowser.open('https://github.com/JeffyNarwhal/randomNumberGen', new=1)
     if event == "Gen":
-        Max = values[1]
-        Min = values[0]
+        try: 
+            Min, Max = int(values[0]), int(values[1])
+        except:
+            window["Text"].update("One or more values are not an integer")
+            continue
         for i in range(int(values[2])):
-            if int(Min) > int(Max):
-                Min = values[1]
-                Max = values[0]
+            if Min > Max:
+                Min, Max = Max, Min
             n = random.randint(int(Min), int(Max))
             randomlist.append(n)
             window["Text"].update(randomlist)
